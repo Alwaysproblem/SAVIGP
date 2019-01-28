fix the msse formula

example.py
@@ -52,6 +52,7 @@ gp = Savigp(likelihood=likelihood,
 
 # Now fit the model to our training data.
 gp.fit(train_inputs, train_outputs, 
+        # num_threads=4,
         optimize_stochastic = False, 
         optimization_config={'mog': 25, 'hyp': 25, 'll': 25, 'inducing': 8})
 
@@ -65,5 +66,8 @@ predicted_var = transform.untransform_Y_var(predicted_var)
 
 # Print the mean standardized squared error.
 test_outputs = data['test_outputs']
-print("MSSE:", (((predicted_mean - test_outputs) ** 2).mean() /
-                ((test_outputs.mean() - test_outputs) ** 2).mean()))
+
+print("Root Mean Square Error:", np.sqrt(((predicted_mean - test_outputs)**2).mean()))
+print("Mean Standardized Error:", ((predicted_mean - test_outputs) / predicted_var).mean())
+print("Root Mean Square Standardized Error:", np.sqrt(((predicted_mean - test_outputs) ** 2).mean() /
+                                                        ((test_outputs.mean() - test_outputs) ** 2).mean()))

predtest.py
@@ -60,13 +60,13 @@ print("define a kernel")
 #                  lengthscale=np.array([1.0]),
 #                  ARD=False)]
 
-kernel = get_kernels(data["train_inputs"].shape[1], dim, variance=11, lengthscale=np.array((9.,)))
+kernel = get_kernels(data["train_inputs"].shape[1], dim, variance=5, lengthscale=np.array((5.,)))
 # kernel = [ExtRBF(data['train_inputs'].shape[1], variance=11, lengthscale=np.array((9.,)), ARD=False)
 #             for _ in range(10)]
 
 # Set the number of inducing points to be half of the training data.
 # num_inducing = int(0.5 * data['train_inputs'].shape[0])
-num_inducing = int(0.007 * data['train_inputs'].shape[0])
+num_inducing = int(0.04 * data['train_inputs'].shape[0])
 
 # Transform the data before training.
 print("data transformation")
@@ -79,11 +79,11 @@ test_inputs = transform.transform_X(data['test_inputs'])
 # Initialize the model.
 print("initialize the model")
 gp = Savigp(
-            
             likelihood=likelihood,
             kernels=kernel,
             num_inducing=num_inducing,
-            random_inducing=True,
+            # random_inducing=True,
+            partition_size=3000,
             debug_output=True
         )
 
@@ -92,7 +92,7 @@ print("fitting")
 gp.fit(
         train_inputs, 
         train_outputs, 
-        optimization_config={'hyp': 15}, 
+        optimization_config={'hyp': 15, "mog": 60, "inducing": 15},
         max_iterations=300, 
         optimize_stochastic=False
     )

savigp/optimizer.py
@@ -136,7 +136,10 @@ class BatchModelWrapper(object):
 
     def objective_function(self, new_params):
         self.update(new_params)
-        model_logging.logger.debug('Objective: %.4f', self._model.objective_function())
+        # model_logging.logger.debug('Objective: %.4f', self._model.objective_function())
+        model_logging.logger.debug('Neg ELBO: %.4f - Neg KL: %.4f - Neg ELL: %.4f', self._model.objective_function(),
+                                   -(self._model.cached_entropy + self._model.cached_cross),
+                                   -self._model.cached_ell)
         return self._model.objective_function().astype(np.float64)
 
     def objective_function_gradients(self, new_params):
@@ -211,12 +214,14 @@ def stochastic_optimize_model(model, optimization_config, max_iterations=200, mo
         model_logging.logger.info('Last objective value: %f', model.overall_objective_function())
 
     end = time.time()
-    return (end - start), total_evaluations 
+    return (end - start), total_evaluations
+
 grad_rms = [0] * 10
 change_rms = [0] * 10
 m_t = [0] * 10
 v_t = [0] * 10
 t = [0] * 10
+
 def sgd(model, num_batches, max_passes, idx):
     """
     Optimise the model using mini-batch stochastic gradient descent.

savigp/util.py
@@ -95,8 +95,8 @@ def KL_normal(m1, sigma1, m2, sigma2):
     N(``mu1``, ``sigma1``), N(``mu2``, ``sigma2``)
     """
 
-    return 1. / 2. * (math.log(det(sigma2) / det(sigma1)) - len(m1) + trace(mdot(inv(sigma2), sigma1)) + \
-    mdot((m2 - m1).T, inv(sigma2) , m2- m1))
+    return 1 / 2 * (math.log(det(sigma2) / det(sigma1)) - len(m1) + trace(mdot(inv(sigma2), sigma1)) + \
+            mdot((m2 - m1).T, inv(sigma2) , m2- m1))
 
 
 def cross_ent_normal(m1, sigma1, m2, sigma2):
@@ -105,7 +105,7 @@ def cross_ent_normal(m1, sigma1, m2, sigma2):
     N(``mu1``, ``sigma1``), N(``mu2``, ``sigma2``)
     """
 
-    return -KL_normal(m1, sigma1, m2, sigma2) - 1. / 2 * math.log(det(2.0 * math.pi * math.e * sigma1))
+    return -KL_normal(m1, sigma1, m2, sigma2) - 1 / 2 * math.log(det(2 * math.pi * math.e * sigma1))
 
 
 def jitchol(A, maxtries=5):
@@ -170,8 +170,8 @@ def log_diag_gaussian(m1, m2, s_log):
 
     N(m1| m2, exp(s_log)), where the covariance matrix is diagonal
     """
-    const = -1.0 / 2 * s_log.sum() - float(len(s_log)) / 2 * np.log(2 * math.pi)
-    return const + -1.0 / 2 * np.dot((m1 - m2) / np.exp(s_log), (m1-m2).T)
+    const = -1 / 2 * s_log.sum() - float(len(s_log)) / 2 * np.log(2 * math.pi)
+    return const + -1 / 2 * np.dot((m1 - m2) / np.exp(s_log), (m1-m2).T)
 
 
 class bcolors:
@@ -243,10 +243,6 @@ def drange(start, stop, step):
     while r < stop:
         yield r
         r += step
-
-
-# class JitChol(Exception):
-#     pass
 
 class JitChol(Exception):
     def __init__(self, message):